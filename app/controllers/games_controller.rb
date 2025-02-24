6e11ebc10913983232fe894f722bc9da806f7450
Dulcelene Machado <dudutola@gmail.com>
2025-02-24 11:59:32 +0000

    Implemented score action with word validation and dictionary lookup using API

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,30 @@
+require "open-uri"
+require "json"
+
 class GamesController < ApplicationController
   def new
     @letters = ("A".."Z").to_a.sample(10)
+  end
+
+  def score
+    @word = params[:word].upcase
+    letters = params[:letters]
+
+    valid_word = @word.chars.all? { |char| letters.include?(char) } &&
+    @word.chars.all? { |char| @word.count(char) <= letters.count(char) }
+
+    if valid_word
+      url = "https://dictionary.lewagon.com/#{@word}"
+      response = URI.open(url).read
+      data = JSON.parse(response)
+
+      if data["found"]
+        @result = "Congratulations! #{@word} is a valid English word!"
+      else
+        @result = "Sorry but #{@word} does not seem to be a valid English word..."
+      end
+    else
+      @result = "Sorry but #{@word} can't be built out of #{letters.join(", ")}"
+    end
   end
 end
